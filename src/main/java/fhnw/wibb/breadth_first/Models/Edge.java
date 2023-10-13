a58ca0f7323b13047b9377f6649bd940cd76237d
MadMowgli <laeubin.j@gmail.com>
2023-10-13 08:11:50 +0200

    Created models

---
+++ src/main/java/fhnw/wibb/breadth_first/Models/Edge.java
@@ -0,0 +1,28 @@
+package fhnw.wibb.breadth_first.Models;
+
+public class Edge {
+
+    // Fields
+    private Node a;
+    private Node b;
+
+
+    // Getters & Setters
+
+
+    public Node getA() {
+        return a;
+    }
+
+    public void setA(Node a) {
+        this.a = a;
+    }
+
+    public Node getB() {
+        return b;
+    }
+
+    public void setB(Node b) {
+        this.b = b;
+    }
+}
---
+++ src/main/java/fhnw/wibb/breadth_first/Models/Node.java
@@ -0,0 +1,16 @@
+package fhnw.wibb.breadth_first.Models;
+
+public class Node {
+
+    // Fields
+    private String name;
+
+    // Getters & Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
