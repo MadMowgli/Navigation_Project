85fcc89a15cdc5c731f7e782feb15c3c1b9dfe2c
Joel <laeubin.j@gmail.com>
2023-10-15 14:35:21 +0200

    Fixed depth search

--- src/main/java/fhnw/wibb/depth_first/DepthFirstSearch.java
+++ src/main/java/fhnw/wibb/depth_first/DepthFirstSearch.java
@@ -1,33 +1,39 @@
 package fhnw.wibb.depth_first;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+
+import java.util.*;
+
 public class DepthFirstSearch {
 
-    public static List<Node> depthFirstSearch(Node startNode, String targetNodeName, Set<Node> visited) {
-        if (startNode == null) return null;
-        visited.add(startNode);
+    public static ArrayList<Node> findPath(Node startNode, Node destinationNode) {
 
-        if (startNode.getName().equals(targetNodeName)) {
-            List<Node> path = new ArrayList<>();
-            path.add(startNode);
-            return path;
-        }
+        // Initialize stuff
+        Stack<ArrayList<Node>> stack = new Stack<>();
+        ArrayList<Node> initialPath = new ArrayList<>();
+        initialPath.add(startNode);     // Add starting node to the path, since we begin from here
 
-        for (Node neighbor : startNode.getNeighbours()) {
-            if (!visited.contains(neighbor)) {
-                List<Node> path = depthFirstSearch(neighbor, targetNodeName, visited);
-                if (path != null) {
-                    path.add(0, startNode);  // Add the current node to the path
-                    return path;
+        stack.push(initialPath);
+
+        while (!stack.isEmpty()) {
+            ArrayList<Node> currentPath = stack.pop();
+            Node currentNode = currentPath.get(currentPath.size() - 1); // Grabs the last node of the current path
+
+            if (currentNode.equals(destinationNode)) {
+                return currentPath; // Path found
+            }
+
+            for (Node neighbour : currentNode.getNeighbours()) {
+                if (!currentPath.contains(neighbour)) {
+                    ArrayList<Node> newPath = new ArrayList<>(currentPath);
+                    newPath.add(neighbour);
+                    stack.push(newPath);
                 }
             }
         }
 
-        return null;
+        return null; // Path not found
+
+
     }
-
-
 }
 
 
--- src/main/java/fhnw/wibb/depth_first/Driver.java
+++ src/main/java/fhnw/wibb/depth_first/Driver.java
@@ -1,4 +1,5 @@
 package fhnw.wibb.depth_first;
+import fhnw.wibb.breadth_first.Models.BreadthFirst;
 import fhnw.wibb.depth_first.Node;
 import fhnw.wibb.depth_first.Loader;
 
@@ -9,6 +10,17 @@ public class Driver {
     public static void main(String[] args) {
 
         ArrayList<Node> nodeList = Loader.load();
+        Node start = nodeList.get(0);
+        Node end = nodeList.get(10);
+        ArrayList<Node> path = DepthFirstSearch.findPath(start, end);
+
+        System.out.println("/////////////////////////////// DEPTH FIRST SEARCH RESULTS");
+        System.out.println("///////// STARTING NODE: " + start.getName());
+        System.out.println("///////// ENDING NODE: " + end.getName());
+        System.out.println("///////// PATH: ");
+        path.stream().forEach(node -> System.out.println("//// " + node.getName() + " ->"));
+        System.out.println("///////////////////////////////");
+
         System.out.println();
     }
 
