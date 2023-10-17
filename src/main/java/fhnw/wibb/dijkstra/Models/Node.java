d5c04bf988e2147aea2d330a30e706fed71bd23c
margareta karaqi <margareta.karaqi@students.fhnw.ch>
2023-10-17 18:34:28 +0200

    extended Class Node

--- src/main/java/fhnw/wibb/dijkstra/Models/Node.java
+++ src/main/java/fhnw/wibb/dijkstra/Models/Node.java
@@ -1,4 +1,38 @@
 package fhnw.wibb.dijkstra.Models;
 
+import java.util.ArrayList;
+
 public class Node {
+    private String name;
+    private ArrayList<Node> neighbours;
+    private Node parent;
+
+    public Node(String name){
+        this.name = name;
+        this.neighbours = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Node> getNeighbours() {
+        return neighbours;
+    }
+
+    public void setNeighbours(ArrayList<Node> neighbours) {
+        this.neighbours = neighbours;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
 }
