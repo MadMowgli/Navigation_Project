9e9a1f03789c257eddd14f1e393a3475ef7f15c5
MadMowgli <laeubin.j@gmail.com>
2023-10-19 15:16:50 +0200

    Adapted WatchDog class to create memory snapshots for measuring memory consumption.

--- src/main/java/fhnw/wibb/util/WatchDog.java
+++ src/main/java/fhnw/wibb/util/WatchDog.java
@@ -3,43 +3,55 @@ package fhnw.wibb.util;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 
 public class WatchDog {
 
     // Fields
     private final HashMap<String, HashMap<String, Long>> measurements;
     private final ArrayList<String> acceptedFormats;
+    private final String algorithmName;
 
     // Constructor
-    public WatchDog() {
+    public WatchDog(String algorithmName) {
+        this.algorithmName = algorithmName;
         this.measurements = new HashMap<>();
+        measurements.put(algorithmName, new HashMap<>());
         this.acceptedFormats = new ArrayList<>(Arrays.asList("ns", "ms", "s"));
     }
 
 
     // Methods
-    // This method "starts" a new timer with the given name
-    public void startMeasurement(String name) {
-        measurements.put(name, new HashMap<>());
-        measurements.get(name).put("start", System.nanoTime());
+    // This method "starts" a new timer with the given algorithmName
+    public void startTime() {
+        measurements.get(algorithmName).put("start", System.nanoTime());
     }
 
     // This method "stops" an existing timer and returns (end - start), which is the measured time in nanoseconds
-    public long stopMeasurement(String name) {
-        if(!measurements.containsKey(name)) { throw new RuntimeException("No Measurement found with key: " + name); }
-        measurements.get(name).put("stop", System.nanoTime());
-        measurements.get(name).put("total", measurements.get(name).get("stop") - measurements.get(name).get("start"));
-        return measurements.get(name).get("total");
+    public long stopTime() {
+        if(!measurements.containsKey(algorithmName)) { throw new RuntimeException("No Measurement found with key: " + algorithmName); }
+        measurements.get(algorithmName).put("stop", System.nanoTime());
+        measurements.get(algorithmName).put("total", measurements.get(algorithmName).get("stop") - measurements.get(algorithmName).get("start"));
+        return measurements.get(algorithmName).get("total");
     }
 
-    public double getMeasurement(String name, String format) {
-        if(!measurements.containsKey(name)) { throw new RuntimeException("No Measurement found with key: " + name); }
+    public void snapShotFreeMemory() {
+        String measurementName = "FreeMemory_" + System.currentTimeMillis();
+        measurements.get(algorithmName).put(measurementName, Runtime.getRuntime().freeMemory());
+    }
+
+    public void snapShotTotalMemory() {
+        String measurementName = "TotalMemory" + System.currentTimeMillis();
+        measurements.get(algorithmName).put(measurementName, Runtime.getRuntime().totalMemory());
+    }
+
+
+    public double getMeasurement(String format) {
+        if(!measurements.containsKey(algorithmName)) { throw new RuntimeException("No Measurement found with key: " + algorithmName); }
         if(!acceptedFormats.contains(format)) { throw new IllegalArgumentException("Time format '"+format+"' not allowed! Use ns, ms or s"); }
         return switch (format) {
-            case "ns" -> measurements.get(name).get("total");
-            case "ms" -> measurements.get(name).get("total") / 1000000.0;
-            case "s" -> measurements.get(name).get("total") / 1000000000.0;
+            case "ns" -> measurements.get(algorithmName).get("total");
+            case "ms" -> measurements.get(algorithmName).get("total") / 1000000.0;
+            case "s" -> measurements.get(algorithmName).get("total") / 1000000000.0;
             default -> -1;
         };
     }
